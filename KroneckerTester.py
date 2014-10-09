a31ad99a4208560ba3f14b08d9ad3098acae3374
Benjamin D Horne <benjamindhorne314@gmail.com>
2014-10-09 13:18:06 -0400

    Update KroneckerTester.py
    Update of tester using new Custom Stochastic Seed Matrix Method.

--- KroneckerTester.py
+++ KroneckerTester.py
@@ -25,22 +25,28 @@ def create_graph_stats(nxgraph):
     return stats #conn,
 
 #above are methods to make input for histogram
-nodes = 2
+nodes = 4
 init = InitMatrix(nodes)
 init.make()
-init.addEdge(0, 1)
-for i in range(nodes):
-        init.addEdge(i, i)#self edges
-init.makeStochastic(0.6, 0.5)
+#init.addEdge(0, 1)
+#init.addEdge(0, 2)
+#init.addEdge(0, 3)
+#for i in range(nodes):
+#        init.addEdge(i, i)#self edges
+p = .45
+c = 2
+probArr = np.array([1, p*c, p*c, p*c, p*c, 1, p, p, p*c, p, 1, 0, p*c, p, 0, 1])
+init.makeStochasticCustom(probArr) #pref attach
 
-k = 5
+k = 3
 print "Seed Matrix Nodes:"
 print nodes
 print "Kronecker Iterations:"
 print k
-nxgraph = Generator.generateStochasticKron(init, k)
+nxgraph = Generator.generateStochasticKron(init, k, True)
+#nx.draw_networkx(nxgraph) #not working?
 #for line in nx.generate_edgelist(nxgraph, data=False):
-    #print(line)
+ #   print(line)
 print "Done Creating Network!"
 print "Creating Histogram..."
 histogramInput = create_graph_stats(nxgraph)
